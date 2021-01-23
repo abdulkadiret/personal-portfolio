b361a440b00b0e28f7f31516f74d10c4adbac340
abdulkadiret <ethiogentelman@gmail.com>
2021-01-23 12:46:17 +0000

    Add footer content and style

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './style.css';
 
 const Footer = () => {
-  return <section id='footer'></section>;
+  return (
+    <footer class='footer'>
+      <div className='footer__copyright'>
+        <span>Copyright © AkeyDev 2021</span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
