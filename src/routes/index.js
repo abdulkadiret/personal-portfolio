33f18278f1a34bf30b414d25740ee72d840ae55f
abdulkadiret <ethiogentelman@gmail.com>
2021-01-25 21:04:34 +0000

    Handle 404 page

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Content from '../content';
 import Navbar from '../components/Navbar';
+import NotFoundPage from '../components/404Page';
 
 export default () => (
   <>
     <Navbar />
     <Switch>
       <Route exact path='/' component={Content} />
+      <Route path='*' component={NotFoundPage} />
     </Switch>
   </>
 );
