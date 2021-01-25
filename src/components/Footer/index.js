6a03f435d1644bbce6f9660640b0966eb7c62444
abdulkadiret <ethiogentelman@gmail.com>
2021-01-25 17:27:03 +0000

    Add social icons to footer - visible only on md & down

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,71 @@
 import React from 'react';
 import './style.css';
+import Github from '../../assets/images/github.svg';
+import Linkedin from '../../assets/images/linkedin.svg';
+import Twitter from '../../assets/images/twitter.svg';
+import Facebook from '../../assets/images/facebook.svg';
 
 const Footer = () => {
   return (
     <footer id='footer'>
-      <div className='footer__copyright'>
-        <span>Copyright © AkeyDev 2021</span>
+      <div className='footer__content py-2'>
+        <div className='footer__copyright'>
+          <span>Copyright © AkeyDev 2021</span>
+        </div>
+        <ul className='footer__social__content d-lg-none'>
+          <li className='mx-2'>
+            <a
+              href='https://github.com/abdulkadiret'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <img
+                src={Github}
+                className='social__icons'
+                alt='github'
+                aria-hidden='true'
+              />
+            </a>
+          </li>
+          <li className='mx-2'>
+            <a
+              href='https://www.linkedin.com/in/abdulkadir-awel-23781a1a4/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <img
+                src={Linkedin}
+                className='social__icons'
+                alt='linkedin'
+                aria-hidden='true'
+              />
+            </a>
+          </li>
+          <li className='mx-2'>
+            <a
+              href='https://twitter.com/home'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <img
+                src={Twitter}
+                className='social__icons'
+                alt='twitter'
+                aria-hidden='true'
+              />
+            </a>
+          </li>
+          <li className='mx-2'>
+            <a href='#' target='_blank' rel='noopener noreferrer'>
+              <img
+                src={Facebook}
+                className='social__icons'
+                alt='facebook'
+                aria-hidden='true'
+              />
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   );
