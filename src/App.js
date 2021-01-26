0e335557ae7941f8826f9b8e0dc2cf4ab431e63a
abdulkadiret <ethiogentelman@gmail.com>
2021-01-26 01:41:03 +0000

    install aos -animate on scroll  package and initalse

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const App = () => {
+  useEffect(() => {
+    AOS.init({ duration: 2000, once: true });
+  }, []);
   return (
     <Router>
       <Routes />
