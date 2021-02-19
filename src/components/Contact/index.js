a96797a394e004f29ab2acca114102d61b2f0be9
abdulkadiret <ethiogentelman@gmail.com>
2021-02-19 11:32:27 +0000

    Change form with Formik & add Yup form validation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,31 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './style.css';
 import { Container, Form, Col, Card } from 'react-bootstrap';
 import TextareaAutosize from 'react-autosize-textarea';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+// Schema for yup
+const validationSchema = Yup.object().shape({
+  fullname: Yup.string().trim().max(50, 'Too long!').required('Required'),
+  email: Yup.string()
+    .trim()
+    .email('Invalid email address')
+    .max(100, 'Too long!')
+    .required('Required'),
+  subject: Yup.string().trim().max(80, 'Too long!').required('Required'),
+  message: Yup.string().trim().required('Required'),
+});
 
 const Contact = ({ className }) => {
-  const [userInputData, setUserInputData] = useState({
-    fullname: '',
-    email: '',
-    subject: '',
-    message: '',
-  });
-
-  const handleChange = (event) => {
-    const UpdatedUserInputData = {
-      ...userInputData,
-      [event.target.name]: event.target.value,
-    };
-    setUserInputData(UpdatedUserInputData);
-  };
-
-  const submit = () => {
-    console.log(`Name = ${userInputData.fullname}`);
-    console.log(`Email = ${userInputData.email}`);
-    console.log(`Subject = ${userInputData.subject}`);
-    console.log(`Message = ${userInputData.message}`);
-  };
-
   return (
     <section id='contact' className={className}>
       <Container className='contact__content px-sm-1 px-md-5 px-lg-1 px-xl-5 col-lg-8 col-lg-offset-2'>
@@ -38,99 +30,160 @@ const Contact = ({ className }) => {
               <span className='d-block'>feel free to shoot me an email!</span>
             </p>
           </div>
-          <>
-            <Form onSubmit={submit}>
-              <Form.Row>
-                <Form.Group
-                  as={Col}
-                  md={5}
-                  controlId='formGridFullname'
-                  className='form__group'
+          <Formik
+            initialValues={{
+              fullname: '',
+              email: '',
+              subject: '',
+              message: '',
+            }}
+            validationSchema={validationSchema}
+            onSubmit={(values, { setSubmitting, resetForm }) => {
+              setSubmitting(true);
+              setTimeout(() => {
+                resetForm();
+                setSubmitting(false);
+              }, 500);
+            }}
+          >
+            {(formik) => (
+              <Form onSubmit={formik.handleSubmit}>
+                <Form.Row>
+                  <Form.Group
+                    as={Col}
+                    md={5}
+                    controlId='formGridFullname'
+                    className='form__group'
+                    data-aos='fade-up'
+                  >
+                    <Form.Control
+                      required
+                      type='text'
+                      name='fullname'
+                      placeholder='Enter Full Name'
+                      className={
+                        formik.touched.fullname && formik.errors.fullname
+                          ? 'form__control error'
+                          : 'form__control'
+                      }
+                      {...formik.getFieldProps('fullname')}
+                    />
+                    <Form.Label htmlFor='fullname' className='form__label'>
+                      Name<span aria-hidden='true'></span>
+                    </Form.Label>
+                    {formik.touched.fullname && formik.errors.fullname ? (
+                      <p className='error__message'>{formik.errors.fullname}</p>
+                    ) : null}
+                  </Form.Group>
+                  <Form.Group
+                    as={Col}
+                    md={5}
+                    controlId='formGridEmail'
+                    className='form__group ml-auto'
+                    data-aos='fade-up'
+                  >
+                    <Form.Control
+                      required
+                      type='email'
+                      name='email'
+                      placeholder='Enter Email'
+                      className={
+                        formik.touched.email && formik.errors.email
+                          ? 'form__control error'
+                          : 'form__control'
+                      }
+                      {...formik.getFieldProps('email')}
+                    />
+                    <Form.Label htmlFor='email' className='form__label'>
+                      Email<span aria-hidden='true'></span>
+                    </Form.Label>
+                    {formik.touched.email && formik.errors.email ? (
+                      <p className='error__message'>{formik.errors.email}</p>
+                    ) : null}
+                  </Form.Group>
+                </Form.Row>
+                <Form.Row>
+                  <Form.Group
+                    as={Col}
+                    md={12}
+                    controlId='formGridSubject'
+                    className='form__group'
+                    data-aos='fade-up'
+                  >
+                    <Form.Control
+                      required
+                      type='text'
+                      name='subject'
+                      placeholder='Enter Subject'
+                      className={
+                        formik.touched.subject && formik.errors.subject
+                          ? 'form__control error'
+                          : 'form__control'
+                      }
+                      {...formik.getFieldProps('subject')}
+                    />
+                    <Form.Label
+                      htmlFor='subject'
+                      className='form__label subject__label__ref'
+                    >
+                      Subject<span aria-hidden='true'></span>
+                    </Form.Label>
+                    {formik.touched.subject && formik.errors.subject ? (
+                      <p className='error__message'>{formik.errors.subject}</p>
+                    ) : null}
+                  </Form.Group>
+                </Form.Row>
+                <Form.Row>
+                  <Form.Group
+                    as={Col}
+                    md={12}
+                    controlId='formGridMessage'
+                    className='form__group'
+                    id='text__area'
+                    data-aos='fade-up'
+                  >
+                    <TextareaAutosize
+                      required
+                      as='textarea'
+                      rows={1}
+                      type='text'
+                      name='message'
+                      placeholder='Write Message'
+                      className={
+                        formik.touched.message && formik.errors.message
+                          ? 'form__control error textarea'
+                          : 'form__control'
+                      }
+                      {...formik.getFieldProps('message')}
+                    />
+                    <Form.Label
+                      htmlFor='message'
+                      className='form__label textarea__label__ref'
+                    >
+                      Message<span aria-hidden='true'></span>
+                    </Form.Label>
+                    {formik.touched.message && formik.errors.message ? (
+                      <p className='error__message textarea__error__msg'>
+                        {formik.errors.message}
+                      </p>
+                    ) : null}
+                  </Form.Group>
+                </Form.Row>
+                <div
+                  className='d-flex justify-content-center col-md-4 col-sm-12 m-auto py-1'
                   data-aos='fade-up'
+                  data-aos-anchor='#text__area'
                 >
-                  <Form.Control
-                    required
-                    type='text'
-                    name='fullname'
-                    placeholder='Enter Full Name'
-                    className='form__control'
-                    value={userInputData.name}
-                    onChange={handleChange}
-                  />
-                  <Form.Label htmlFor='fullname' className='form__label'>
-                    Name<span aria-hidden='true'>*</span>
-                  </Form.Label>
-                </Form.Group>
-                <Form.Group
-                  as={Col}
-                  md={5}
-                  controlId='formGridEmail'
-                  className='form__group ml-auto'
-                  data-aos='fade-up'
-                >
-                  <Form.Control
-                    required
-                    type='email'
-                    name='email'
-                    placeholder='Enter Email'
-                    className='form__control'
-                    value={userInputData.email}
-                    onChange={handleChange}
-                  />
-                  <Form.Label htmlFor='email' className='form__label'>
-                    Email<span aria-hidden='true'>*</span>
-                  </Form.Label>
-                </Form.Group>
-              </Form.Row>
-              <Form.Group className='form__group' data-aos='fade-up'>
-                <Form.Control
-                  required
-                  type='text'
-                  name='subject'
-                  placeholder='Enter Subject'
-                  className='form__control'
-                  value={userInputData.subject}
-                  onChange={handleChange}
-                />
-                <Form.Label
-                  htmlFor='subject'
-                  className='form__label subject__label__ref'
-                >
-                  Subject<span aria-hidden='true'>*</span>
-                </Form.Label>
-              </Form.Group>
-              <Form.Group
-                className='form__group'
-                id='text__area'
-                data-aos='fade-up'
-              >
-                <TextareaAutosize
-                  required
-                  as='textarea'
-                  rows={2}
-                  type='text'
-                  name='message'
-                  placeholder='Write Message'
-                  className='form__control'
-                  value={userInputData.message}
-                  onChange={handleChange}
-                />
-                <Form.Label
-                  htmlFor='message'
-                  className='form__label textarea__label'
-                >
-                  Message<span aria-hidden='true'>*</span>
-                </Form.Label>
-              </Form.Group>
-              <div
-                className='d-flex justify-content-center col-md-4 col-sm-12 m-auto py-1'
-                data-aos='fade-up'
-                data-aos-anchor='#text__area'
-              >
-                <button className='send__btn btn'>Send Message</button>
-              </div>
-            </Form>
-          </>
+                  <button
+                    className='send__btn btn'
+                    disabled={!formik.isValid || formik.isSubmitting}
+                  >
+                    Send Message
+                  </button>
+                </div>
+              </Form>
+            )}
+          </Formik>
         </Card>
       </Container>
     </section>
