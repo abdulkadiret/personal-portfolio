f365d4f82a71da5822adfa214e7938ce26a634ed
abdulkadiret <ethiogentelman@gmail.com>
2021-04-06 17:20:50 +0100

    Refactor the server code

---
+++ mail.js
@@ -0,0 +1,76 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+//auth config object
+const auth = {
+  type: 'OAuth2',
+  user: process.env.MAIL_USERNAME,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: process.env.REDIRECT_URI,
+  refreshToken: process.env.REFRESH_TOKEN,
+};
+
+//create transport
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: auth,
+});
+
+const sendEmail = (fullname, email, subject, message, callBack) => {
+  const htmlEmail = `
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<style>
+h3{
+font-size: 14px;
+color: #800080;
+}
+p{
+white-space: pre-wrap;
+}
+span{
+font-weight: bold;
+}
+ul {
+list-style-type: none;
+}
+</style>
+</head>
+<body>
+<p>You have a new contact request:</p>
+<h3>Contact Details</h3>
+<ul>
+<li><span>Full Name:</span> ${fullname}</li>
+<li><span>Email:</span> ${email}</li>
+</ul>
+<p>${message}</p>
+</body>
+<html/>
+`;
+
+  // send mail with defined transport object
+  let mailOptions = {
+    from: `${process.env.SENDER_NAME} 👻 <${email}>`,
+    to: `${process.env.RECEIVER_NAME} <${process.env.MAIL_USERNAME}>`,
+    subject, // Subject line
+    text: message, // plain text body
+    html: htmlEmail,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      callBack(error, null);
+    } else {
+      console.log(info);
+      callBack(null, info);
+    }
+    transporter.close();
+  });
+};
+
+module.exports = sendEmail;
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
 //get express
 const express = require('express');
 //initialise express
 const app = express();
-const nodemailer = require('nodemailer');
+const sendEmail = require('./mail');
 
 const path = require('path');
 
@@ -13,87 +12,29 @@ app.use(json());
 app.use(urlencoded({ extended: true }));
 
 app.post('/api/sendmail', (req, res) => {
+  // expect to receive some data from client
   const { fullname, email, subject, message } = req.body;
 
-  const htmlEmail = `
-<!DOCTYPE html>
-<html>
-  <head>
-      <meta name="viewport" content="width=device-width" />
-      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <style>
-      h3{
-      font-size: 14px;
-      color: #800080;
-      }
-      p{
-      white-space: pre-wrap;
-      }
-      span{
-      font-weight: bold;
-      }
-      ul {
-      list-style-type: none;
-      }
-  </style>
-  </head>
-  <body>
-      <p>You have a new contact request:</p>
-      <h3>Contact Details</h3>
-      <ul>
-          <li><span>Full Name:</span> ${fullname}</li>
-          <li><span>Email:</span> ${email}</li>
-      </ul>
-      <p>${message}</p>
-  </body>
-<html/>
-`;
-
-  // send mail with defined transport object
-  let mailOptions = {
-    from: `${process.env.SENDER_NAME} 👻 <${email}>`,
-    to: `${process.env.RECEIVER_NAME} <${process.env.MAIL_USERNAME}>`,
-    subject, // Subject line
-    text: message, // plain text body
-    html: htmlEmail,
-  };
-
-  //auth config object
-  const auth = {
-    type: 'OAuth2',
-    user: process.env.MAIL_USERNAME,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    redirectUri: process.env.REDIRECT_URI,
-    refreshToken: process.env.REFRESH_TOKEN,
-  };
-
-  //create transport
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: auth,
-  });
-
-  transporter.sendMail(mailOptions, (error, data) => {
-    if (error) {
-      res.status(500).send({
-        success: false,
-        message: 'Internal error, Please try again later',
-      });
+  sendEmail(fullname, email, subject, message, (err, info) => {
+    if (err) {
+      res
+        .status(500)
+        .send({ message: 'Internal Error, Please try again later' });
     } else {
       res.send({
-        success: true,
         message: 'Message successfully send!!!',
+        info,
       });
     }
   });
-  transporter.close();
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  //serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //Handle react routing, all requests to React App
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
