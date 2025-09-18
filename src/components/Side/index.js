c183c890da3da07722dcc92db07856362991ffdc
abdulkadiret <ethiogentelman@gmail.com>
2025-09-18 18:05:29 +0100

    Updated SideElements with React Icons and cleaner styling

--- src/components/Side/index.js
+++ src/components/Side/index.js
@@ -1,196 +1,120 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './style.css';
-import Github from '../../assets/images/github.svg';
-import Linkedin from '../../assets/images/linkedin.svg';
-import Twitter from '../../assets/images/twitter.svg';
-import Facebook from '../../assets/images/facebook.svg';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faChevronLeft,
-  faChevronRight,
-} from '@fortawesome/free-solid-svg-icons';
+import { TbBrandGithub } from 'react-icons/tb';
+import { RiTwitterXFill } from 'react-icons/ri';
+import { SlSocialLinkedin } from 'react-icons/sl';
+import { ImCodepen } from 'react-icons/im';
+import { HiArrowRight, HiArrowLeft } from 'react-icons/hi';
 
 const SideElements = () => {
   const [showContent, setShowContent] = useState(true);
   const [isHovered, setIsHovered] = useState(false);
+  const toggleBtnRef = useRef(null);
 
   const toggleSideContents = (e) => {
     e.preventDefault();
-    setShowContent(!showContent);
+    setShowContent((prev) => !prev);
   };
 
-  const elements = document.getElementsByClassName(
-    'side__content__toggle__btn'
-  );
-  const n = elements.length;
-  const changeColor = (color) => {
-    for (let i = 0; i < n; i++) {
-      elements[i].style.color = color;
+  useEffect(() => {
+    if (!toggleBtnRef.current) return;
+
+    if (isHovered) {
+      toggleBtnRef.current.style.color = '#0275d8';
+    } else {
+      toggleBtnRef.current.style.color = '#505050';
     }
-  };
-
-  for (let i = 0; i < n; i++) {
-    elements[i].onmouseover = () => changeColor('#0275d8');
-    elements[i].onmouseout = () => changeColor('#505050');
-  }
+  }, [isHovered]);
 
   return (
-    <aside id='side'>
+    <aside id='side' className='position-relative'>
       <div className='left__side d-none d-lg-block'>
-        {showContent ? (
+        {showContent && (
           <ul className='left__side__socialLinks' data-aos='fade-right'>
-            <li>
-              <a
-                href='https://github.com/abdulkadiret'
-                target='_blank'
-                rel='noopener noreferrer'
-                aria-label='github'
-              >
+            {[
+              {
+                icon: <TbBrandGithub className='social__icons p-1' />,
+                label: 'Github',
+                link: 'https://github.com/abdulkadiret',
+              },
+              {
+                icon: <SlSocialLinkedin className='social__icons p-1' />,
+                label: 'Linkedin',
+                link: 'https://www.linkedin.com/in/abdulkadir-awel-23781a1a4/',
+              },
+              {
+                icon: <RiTwitterXFill className='social__icons p-1' />,
+                label: 'X (Twitter)',
+                link: 'https://x.com/Akey_Awel',
+              },
+              {
+                icon: <ImCodepen className='social__icons p-1' />,
+                label: 'OpenProcessing',
+                link: 'https://openprocessing.org/user/223890#sketches',
+              },
+            ].map((social, idx) => (
+              <li key={idx}>
                 <OverlayTrigger
                   placement='right'
                   trigger={['hover', 'focus']}
-                  overlay={<Tooltip className='right'>Github</Tooltip>}
+                  overlay={<Tooltip>{social.label}</Tooltip>}
                 >
-                  <img
-                    src={Github}
-                    className='social__icons'
-                    alt='github'
-                    aria-hidden='true'
-                  />
+                  <a
+                    href={social.link}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    aria-label={social.label}
+                    className='social__icon__btn'
+                  >
+                    {social.icon}
+                  </a>
                 </OverlayTrigger>
-              </a>
-            </li>
-            <li>
-              <a
-                href='https://www.linkedin.com/in/abdulkadir-awel-23781a1a4/'
-                target='_blank'
-                rel='noopener noreferrer'
-                aria-label='linkedin'
-              >
-                <OverlayTrigger
-                  placement='right'
-                  trigger={['hover', 'focus']}
-                  overlay={<Tooltip className='right'>Linkedin</Tooltip>}
-                >
-                  <img
-                    src={Linkedin}
-                    className='social__icons'
-                    alt='linkedin'
-                    aria-hidden='true'
-                  />
-                </OverlayTrigger>
-              </a>
-            </li>
-            <li>
-              <a
-                href='https://twitter.com/home'
-                target='_blank'
-                rel='noopener noreferrer'
-                aria-label='twitter'
-              >
-                <OverlayTrigger
-                  placement='right'
-                  trigger={['hover', 'focus']}
-                  overlay={<Tooltip className='right'>Twitter</Tooltip>}
-                >
-                  <img
-                    src={Twitter}
-                    className='social__icons'
-                    alt='twitter'
-                    aria-hidden='true'
-                  />
-                </OverlayTrigger>
-              </a>
-            </li>
-            <li>
-              <a
-                href='#'
-                target='_blank'
-                rel='noopener noreferrer'
-                aria-label='facebook'
-              >
-                <OverlayTrigger
-                  placement='right'
-                  trigger={['hover', 'focus']}
-                  overlay={<Tooltip className='right'>Facebook</Tooltip>}
-                >
-                  <img
-                    src={Facebook}
-                    className='social__icons'
-                    alt='facebook'
-                    aria-hidden='true'
-                  />
-                </OverlayTrigger>
-              </a>
-            </li>
+              </li>
+            ))}
           </ul>
-        ) : (
-          ''
         )}
+
         <div className='toggle__side__content' data-aos='fade-right'>
-          {showContent === true ? (
-            <button
-              aria-label='side content toggle button'
-              onClick={toggleSideContents}
-              className={`side__content__toggle__btn ${
-                isHovered ? 'hovered' : ''
-              }`}
-              onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => setIsHovered(false)}
-            >
-              <FontAwesomeIcon icon={faChevronLeft} />
-            </button>
-          ) : (
-            <button
-              aria-label='side content toggle button'
-              onClick={toggleSideContents}
-              className={`side__content__toggle__btn ${
-                isHovered ? 'hovered' : ''
-              }`}
-              onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => setIsHovered(false)}
-            >
-              <FontAwesomeIcon icon={faChevronRight} />
-            </button>
-          )}
+          <button
+            ref={toggleBtnRef}
+            aria-label='Toggle side content'
+            onClick={toggleSideContents}
+            className={`side__content__toggle__btn mt-1 ${
+              isHovered ? 'hovered' : ''
+            }`}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            {showContent ? (
+              <HiArrowLeft className='toggle-icon left mb-0 p-1' />
+            ) : (
+              <HiArrowRight className='toggle-icon right mb-0 p-1' />
+            )}
+          </button>
         </div>
       </div>
       <div className='right__side d-none d-lg-block'>
-        {showContent === true ? (
-          <div className='right__side__mailto' data-aos='fade-left'>
-            <a
-              href='mailto:akey.maneth@gmail.com'
-              target='_blank'
-              rel='noopener noreferrer'
-              className='email__link'
-            >
-              <span className='mailto'>akey.maneth@gmail.com</span>
-            </a>
+        {showContent && (
+          <div
+            className='d-flex flex-column align-items-center'
+            data-aos='fade-left'
+          >
+            <div className='side__line__1'></div>
+            <div className='right__side__mailto'>
+              <a
+                href='mailto:akey.maneth@gmail.com'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='email__link'
+              >
+                <span className='mailto'>akey.maneth@gmail.com</span>
+              </a>
+            </div>
+            <div className='side__line__2'></div>
           </div>
-        ) : (
-          ''
         )}
-        <div className='toggle__side__content' data-aos='fade-left'>
-          {showContent === true ? (
-            <button
-              aria-label='side content toggle button'
-              onClick={toggleSideContents}
-              className='side__content__toggle__btn'
-            >
-              <FontAwesomeIcon icon={faChevronRight} />
-            </button>
-          ) : (
-            <button
-              aria-label='side content toggle button'
-              onClick={toggleSideContents}
-              className='side__content__toggle__btn'
-            >
-              <FontAwesomeIcon icon={faChevronLeft} />
-            </button>
-          )}
-        </div>
       </div>
     </aside>
   );
