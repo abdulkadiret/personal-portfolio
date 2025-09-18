daa0c9b92eae2a6e6a32adbd8d6591e573bde12f
abdulkadiret <ethiogentelman@gmail.com>
2025-09-18 18:12:32 +0100

    Updated Footer component with React Icons and CSS improvements

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,78 +1,52 @@
 import React from 'react';
 import './style.css';
-import Github from '../../assets/images/github.svg';
-import Linkedin from '../../assets/images/linkedin.svg';
-import Twitter from '../../assets/images/twitter.svg';
-import Facebook from '../../assets/images/facebook.svg';
+import { TbBrandGithub } from 'react-icons/tb';
+import { RiTwitterXFill } from 'react-icons/ri';
+import { SlSocialLinkedin } from 'react-icons/sl';
+import { ImCodepen } from 'react-icons/im';
 
 const Footer = () => {
   return (
     <footer id='footer'>
-      <div className='footer__content py-2'>
+      <div className='footer__content'>
         <div className='footer__copyright'>
           <span>Copyright © AkeyDev {new Date().getFullYear()}</span>
         </div>
         <ul className='footer__social__content d-lg-none'>
-          <li className='mx-2 px-1 py-2'>
-            <a
-              href='https://github.com/abdulkadiret'
-              target='_blank'
-              rel='noopener noreferrer'
-              aria-label='github'
-            >
-              <img
-                src={Github}
-                className='social__icons'
-                alt='github'
-                aria-hidden='true'
-              />
-            </a>
-          </li>
-          <li className='mx-2 px-1 py-2'>
-            <a
-              href='https://www.linkedin.com/in/abdulkadir-awel-23781a1a4/'
-              target='_blank'
-              rel='noopener noreferrer'
-              aria-label='linkedin'
-            >
-              <img
-                src={Linkedin}
-                className='social__icons'
-                alt='linkedin'
-                aria-hidden='true'
-              />
-            </a>
-          </li>
-          <li className='mx-2 px-1 py-2'>
-            <a
-              href='https://twitter.com/home'
-              target='_blank'
-              rel='noopener noreferrer'
-              aria-label='twitter'
-            >
-              <img
-                src={Twitter}
-                className='social__icons'
-                alt='twitter'
-                aria-hidden='true'
-              />
-            </a>
-          </li>
-          <li className='mx-2 px-1 py-2'>
-            <a
-              href='#'
-              target='_blank'
-              rel='noopener noreferrer'
-              aria-label='facebook'
-            >
-              <img
-                src={Facebook}
-                className='social__icons'
-                alt='facebook'
-                aria-hidden='true'
-              />
-            </a>
-          </li>
+          {[
+            {
+              icon: <TbBrandGithub className='social__icons p-1' />,
+              label: 'Github',
+              link: 'https://github.com/abdulkadiret',
+            },
+            {
+              icon: <SlSocialLinkedin className='social__icons p-1' />,
+              label: 'Linkedin',
+              link: 'https://www.linkedin.com/in/abdulkadir-awel-23781a1a4/',
+            },
+            {
+              icon: <RiTwitterXFill className='social__icons p-1' />,
+              label: 'X (Twitter)',
+              link: 'https://x.com/Akey_Awel',
+            },
+            {
+              icon: <ImCodepen className='social__icons p-1' />,
+              label: 'Openprocessing',
+              link: 'https://openprocessing.org/user/223890#sketches',
+            },
+          ].map((social, idx) => (
+            <li key={idx} className='mx-2 px-1 py-2'>
+              <a
+                href={social.link}
+                target='_blank'
+                rel='noopener noreferrer'
+                aria-label={social.label}
+                className='social__icon__btn'
+              >
+                {social.icon}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
     </footer>
